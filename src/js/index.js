0e23a7f359f95c72ae966273f5cb657b564c7ff2
Adam Wojnarowski <adam.piotr.wojnarowski@gmail.com>
2020-06-13 19:42:47 +0200

    add addeventlistener to buttons and add localStorage / finsih project

--- src/js/index.js
+++ src/js/index.js
@@ -2,6 +2,17 @@ import '../scss/main.scss';
 
 /* place your code below */
 
-const textarea = document.querySelector('.textarea--js');
+const textEditor = document.querySelector('.textarea--js');
 const loadButton = document.querySelector('.button__load--js');
 const saveButton = document.querySelector('.button__save--js'); 
+
+saveButton.addEventListener('click', () => {
+    const textEditorString = JSON.stringify(textEditor.value);
+    localStorage.setItem('textarea', textEditorString);
+    textEditor.value = '';
+})
+
+loadButton.addEventListener('click', () => {
+    const textEditorObject = JSON.parse(localStorage.getItem('textarea'));
+    textEditor.value = textEditorObject;
+})
